9b8b8b57f5987e907606c566231bfda0c9b67f87
Ann.s <sumoljit002@gmail.com>
2023-07-23 20:42:10 +0700

    validate csv

--- js/validate.js
+++ js/validate.js
@@ -1,32 +1,19 @@
-// function myFunction() {
-//   // Get the value of the input field with id="numb"
-//   let x = document.getElementById("inputTeacherName").value;
-//   // If x is Not a Number or less than one or greater than 10
-//   let text;
-//   if (isNaN(x) || x < 1 || x > 10) {
-//     text = "Input not valid";
-//   } else {
-//     text = "Input OK";
-//   }
-//   document.getElementById("demo").innerHTML = text;
-// }
-
 (function () {
-    'use strict'
-  
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    var forms = document.querySelectorAll('.needs-validation')
-  
-    // Loop over them and prevent submission
-    Array.prototype.slice.call(forms)
-      .forEach(function (form) {
-        form.addEventListener('submit', function (event) {
-          if (!form.checkValidity()) {
-            event.preventDefault()
-            event.stopPropagation()
-          }
-  
-          form.classList.add('was-validated')
-        }, false)
-      })
-  })()
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+
+        form.classList.add('was-validated')
+      }, false)
+    })
+})()
